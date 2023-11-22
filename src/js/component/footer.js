ed1b3dd51a5e15fb01d8d54f378f85815b685a32
omar saadane el meski <o_omar_96@hotmail.com>
2023-11-22 21:12:08 +0000

    react-falta por terminar

---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    return(
+        <Footer>
+        <p>Copyrigth © Your Website 2019</p>
+    </Footer>
+    )
+}
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import Navbar from './navbar.js';
+import Footer from './footer.js';
+import Jumbotron from './jumbotron.js';
+import Card from './card.js';
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text center Container-fluid">
+			<Navbar/>
+			<div className="container">
+				<Jumbotron/>
+				<div className="">
+			 		<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+				</div>
+			</div>
+			<Footer/>
 		</div>
 	);
 };
